6e57e2415898a1ff0f15b0d46917a69e826c41d6
shashikiran918 <85386298+shashikiran918@users.noreply.github.com>
2021-07-05 19:45:12 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+/// Flutter code sample for Dismissible
+
+// This sample shows how you can use the [Dismissible] widget to
+// remove list items using swipe gestures. Swipe any of the list
+// tiles to the left or right to dismiss them from the [ListView].
+
+import 'package:flutter/material.dart';
+
+void main() => runApp( MyApp());
+
+/// This is the main application widget.
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      home: Scaffold(
+        appBar: AppBar(title: const Text("DISMISSBLE")),
+        body: const MyStatefulWidget(),
+      ),
+    );
+  }
+}
+
+/// This is the stateful widget that the main application instantiates.
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  List<int> items = List<int>.generate(100, (int index) => index);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: items.length,
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      itemBuilder: (BuildContext context, int index) {
+        return Dismissible(
+        direction: DismissDirection.startToEnd,
+          child: ListTile(
+            title: Text(
+              'Item ${items[index]}',
+            ),
+          ),
+          background: Container(
+            color: Colors.green,
+          ),
+          key: ValueKey<int>(items[index]),
+          onDismissed: (DismissDirection direction) {
+            setState(() {
+              items.removeAt(index);
+            });
+          },
+        );
+      },
+    );
+  }
+}
